a1ad098827805f4ccf4c3c7d6b7715c26de6d1be
twanknip <twanknip@gmail.com>
2024-03-12 09:53:58 +0100

    Background Fix

--- adminpanel/login.php
+++ adminpanel/login.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Login pagina</title>
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="assets/css/login.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">  
 </head>
 <body>
