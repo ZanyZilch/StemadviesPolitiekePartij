d77994046bd8beaa5801e02eb7c0695d55a7db49
twanknip <twanknip@gmail.com>
2024-03-20 10:19:17 +0100

    font fix

--- adminpanel/pages/overview.php
+++ adminpanel/pages/overview.php
@@ -9,24 +9,18 @@ ob_start();?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Overview</title>
     <link rel="stylesheet" href="../assets/css/overview.css">
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-wmQ6AQ62fTOxy2vC/UN+TTS7d2oSUuU2OpOSDQVuBvUEkTf3zD5tnsNmOOXSJ7vjIJFmiGfBtSF7vOcNZN0xEQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link href="https://fonts.googleapis.com/css2?family=Hedvig+Letters+Serif:opsz@12..24&display=swap" rel="stylesheet"> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-wmQ6AQ62fTOxy2vC/UN+TTS7d2oSUuU2OpOSDQVuBvUEkTf3zD5tnsNmOOXSJ7vjIJFmiGfBtSF7vOcNZN0xEQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <h1 class="text-center mb-4">Partijen Overview</h1>
-
-            <!-- Modal page-->
-            <!-- Button trigger modal -->
             <button id="editModalTrigger" type="button" class="btn btn-primary" data-toggle="modal" data-target="#editModal" style="display: none;">
             Edit
             </button>
-
-            <!-- Partij Modal -->
             <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
@@ -37,10 +31,8 @@ ob_start();?>
                         </button>
                     </div>
                     <div class="modal-body">
-                        <!-- Form for editing party details -->
                         <form id="editForm">
                             <div class="form-group">
-                                <!-- Custom file input -->
                                 <div class="custom-file">
                                     <input type="file" class="custom-file-input" id="editImage" name="editImage">
                                     <label class="custom-file-label" for="editImage">Choose image</label>
@@ -69,7 +61,6 @@ ob_start();?>
                 </div>
             </div>
 
-            <!-- Stelling Modal -->
             <div class="modal fade" id="stellingModal" tabindex="-1" role="dialog" aria-labelledby="stellingModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
@@ -80,7 +71,6 @@ ob_start();?>
                             </button>
                         </div>
                         <div class="modal-body">
-                            <!-- Form for editing stelling details -->
                             <form id="stellingForm">
                                 <div class="form-group">
                                     <label for="stellingId">ID:</label>
@@ -101,7 +91,6 @@ ob_start();?>
                 </div>
             </div>
 
-            <!-- Modal for Meningen -->
             <div class="modal fade" id="editMeningModal" tabindex="-1" role="dialog" aria-labelledby="editMeningModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
@@ -112,7 +101,6 @@ ob_start();?>
                             </button>
                         </div>
                         <div class="modal-body">
-                            <!-- Form for editing Mening -->
                             <form id="editMeningForm">
                                 <div class="form-group">
                                     <label for="editPartij">Partij:</label>
@@ -141,25 +129,21 @@ ob_start();?>
                     </div>
                 </div>
             </div>
-
-            <!-- Add Party Modal -->
             <div class="modal fade" id="addPartyModal" tabindex="-1" role="dialog" aria-labelledby="addPartyModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="addPartyModalLabel">Add New Partij</h5>
+                            <h5 class="modal-title" id="addPartyModalLabel">Voeg nieuwe partij toe</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div class="modal-body">
-                            <!-- Form for adding party details -->
                             <form id="addPartyForm">
                                 <div class="form-group">
-                                    <!-- Custom file input -->
                                     <div class="custom-file">
                                         <input type="file" class="custom-file-input" id="addImage" name="addImage">
-                                        <label class="custom-file-label" for="addImage">Kies image</label>
+                                        <label class="custom-file-label" for="addImage">Kies foto</label>
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -181,7 +165,6 @@ ob_start();?>
             </div>
 
 
-
             <div class="container mt-5">
                 <ul class="nav nav-tabs" role="tablist">
                     <li class="nav-item">
@@ -194,6 +177,8 @@ ob_start();?>
                         <a class="nav-link" data-toggle="tab" href="#meningen">Meningen</a>
                     </li>
                 </ul>
+
+
 
             <div class="tab-content">
                 <div id="partijen" class="container tab-pane active"><br>
@@ -211,10 +196,10 @@ ob_start();?>
                         </thead>
                         <tbody>
 
-                            <!-- Your PHP code to fetch and populate table rows will go here -->
+
+                        
+
                             <?php
-                            // Sample row, replace with PHP code to fetch data from database
-                                    // Fetch and display houses with the same user_ID
                                 $partySql = "SELECT idPartij, naam, beschrijving, image, ST_X(positie) AS latitude, ST_Y(positie) AS longitude FROM partij";
                                 $partyStmt = $verbinding->prepare($partySql);
                                 $partyStmt->execute();
@@ -250,24 +235,12 @@ ob_start();?>
                                 }
                             ?>
 
-                            <!-- <tr>
-                                <td>Sample Image</td>
-                                <td>Sample Name</td>
-                                <td>Sample SQL: POINT</td>
-                                <td>
-                                    <button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button>
-                                    <button type="button" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
-                                </td>
-                            </tr> -->
+                        
                         </tbody>
                     </table>
                 </div>
 
                 <div id="stellingen" class="container tab-pane fade"><br>
-                    <!-- Table for Stellingen -->
-                    <!-- Add your code for Stellingen here -->
-                    <!-- <p>Stellingen Content Goes Here</p> -->
-
                     <div id="stellingen" class="container tab-pane active"><br>
                     <div class="text-center mb-3">
                         <button class="btn btn-success newStellingBtn">+ Voeg stelling toe</button>
@@ -281,6 +254,9 @@ ob_start();?>
                                 </tr>
                             </thead>
                             <tbody>
+
+
+
                             <?php
                                 $partySql = "SELECT * FROM `stelling`";
                                 $partyStmt = $verbinding->prepare($partySql);
@@ -298,17 +274,13 @@ ob_start();?>
                                     echo "</tr>";
                                 }
                             ?>
+
+
                             </tbody>
                         </table>
-
                     </div>
                 </div>
-
                 <div id="meningen" class="container tab-pane fade"><br>
-                    <!-- Table for Meningen -->
-                    <!-- Add your code for Meningen here -->
-                    <!-- <p>Meningen Content Goes Here</p> -->
-
                     <div id="meningen" class="container tab-pane active"><br>
                     <div class="text-center mb-3">
                         <button class="btn btn-success newMeningBtn">+ Voeg mening toe</button>
@@ -368,20 +340,10 @@ ob_start();?>
         </div>
     </div>
 </div>
-
 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs" viewBox="0 0 1422 800"><defs><linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="oooscillate-grad"><stop stop-color="hsl(162, 100%, 58%)" stop-opacity="1" offset="0%"></stop><stop stop-color="hsl(270, 73%, 53%)" stop-opacity="1" offset="100%"></stop></linearGradient></defs><g stroke-width="4" stroke="url(#oooscillate-grad)" fill="none" stroke-linecap="round"><path d="M 0 546 Q 355.5 -100 711 400 Q 1066.5 900 1422 546" opacity="1.00"></path><path d="M 0 525 Q 355.5 -100 711 400 Q 1066.5 900 1422 525" opacity="0.96"></path><path d="M 0 504 Q 355.5 -100 711 400 Q 1066.5 900 1422 504" opacity="0.92"></path><path d="M 0 483 Q 355.5 -100 711 400 Q 1066.5 900 1422 483" opacity="0.89"></path><path d="M 0 462 Q 355.5 -100 711 400 Q 1066.5 900 1422 462" opacity="0.85"></path><path d="M 0 441 Q 355.5 -100 711 400 Q 1066.5 900 1422 441" opacity="0.81"></path><path d="M 0 420 Q 355.5 -100 711 400 Q 1066.5 900 1422 420" opacity="0.77"></path><path d="M 0 399 Q 355.5 -100 711 400 Q 1066.5 900 1422 399" opacity="0.73"></path><path d="M 0 378 Q 355.5 -100 711 400 Q 1066.5 900 1422 378" opacity="0.70"></path><path d="M 0 357 Q 355.5 -100 711 400 Q 1066.5 900 1422 357" opacity="0.66"></path><path d="M 0 336 Q 355.5 -100 711 400 Q 1066.5 900 1422 336" opacity="0.62"></path><path d="M 0 315 Q 355.5 -100 711 400 Q 1066.5 900 1422 315" opacity="0.58"></path><path d="M 0 294 Q 355.5 -100 711 400 Q 1066.5 900 1422 294" opacity="0.54"></path><path d="M 0 273 Q 355.5 -100 711 400 Q 1066.5 900 1422 273" opacity="0.51"></path><path d="M 0 252 Q 355.5 -100 711 400 Q 1066.5 900 1422 252" opacity="0.47"></path><path d="M 0 231 Q 355.5 -100 711 400 Q 1066.5 900 1422 231" opacity="0.43"></path><path d="M 0 210 Q 355.5 -100 711 400 Q 1066.5 900 1422 210" opacity="0.39"></path><path d="M 0 189 Q 355.5 -100 711 400 Q 1066.5 900 1422 189" opacity="0.35"></path><path d="M 0 168 Q 355.5 -100 711 400 Q 1066.5 900 1422 168" opacity="0.32"></path><path d="M 0 147 Q 355.5 -100 711 400 Q 1066.5 900 1422 147" opacity="0.28"></path><path d="M 0 126 Q 355.5 -100 711 400 Q 1066.5 900 1422 126" opacity="0.24"></path><path d="M 0 105 Q 355.5 -100 711 400 Q 1066.5 900 1422 105" opacity="0.20"></path><path d="M 0 84 Q 355.5 -100 711 400 Q 1066.5 900 1422 84" opacity="0.16"></path><path d="M 0 63 Q 355.5 -100 711 400 Q 1066.5 900 1422 63" opacity="0.13"></path><path d="M 0 42 Q 355.5 -100 711 400 Q 1066.5 900 1422 42" opacity="0.09"></path></g></svg>
-
-<!-- jQuery -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-<!-- Javascript Bootstrap -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.2/js/bootstrap.min.js"></script>
-
-<!-- Partij functions -->
 <script>
-    // Add Partij
-
-    // Update Partij
     $(document).ready(function() {
         $('.editPartyBtn').click(function() {
             var row = $(this).closest('tr');
@@ -432,7 +394,6 @@ ob_start();?>
         });
     });
 
-    // Delete Partij
     $(document).ready(function() {
         $('.deletePartyBtn').click(function() {
             var partyId = $(this).data('id');
@@ -454,10 +415,7 @@ ob_start();?>
         });
     });
 </script>
-
-<!-- Stelling functions -->
 <script>
-    // Update Stelling
     $(document).ready(function() {
         $('.editStellingBtn').click(function() {
             var row = $(this).closest('tr');
@@ -489,30 +447,27 @@ ob_start();?>
         });
     });
 
-    // Delete Stelling
-    $(document).ready(function() {
-        $('.deleteStellingBtn').click(function() {
-            var partyId = $(this).data('id');
-            
-            if (confirm("Ben je zeker dat je deze Stelling wil verwijderen?")) {
-                $.ajax({
-                    url: 'delete-stelling.php',
-                    type: 'POST',
-                    data: { id: partyId },
-                    success: function(response) {
-                        console.log(response);
-                        location.reload(); 
-                    },
-                    error: function(xhr, status, error) {
-                        console.error(xhr.responseText);
-                    }
-                });
-            }
-        });
-    });
-</script>
+  $(document).ready(function() {
+    $('.deleteStellingBtn').click(function() {
+        var stellingId = $(this).data('id'); // Corrected variable name
 
-<!-- Mening functions -->
+        if (confirm("Ben je zeker dat je deze Stelling wil verwijderen?")) {
+            $.ajax({
+                url: 'delete-stelling.php',
+                type: 'POST',
+                data: { id: stellingId }, // Corrected variable name
+                success: function(response) {
+                    console.log(response);
+                    location.reload(); 
+                },
+                error: function(xhr, status, error) {
+                    console.error(xhr.responseText);
+                }
+            });
+        }
+    });
+});
+</script>
 <script>
     // Update Mening
     $(document).ready(function() {
@@ -573,7 +528,7 @@ ob_start();?>
                     // Handle error response
                     console.error(xhr.responseText);
                 }
-            });
+            }n
 
             // Close the modal after saving changes
             $('#editMeningModal').modal('hide');
@@ -597,7 +552,7 @@ ob_start();?>
                     data: { idParty: partyId, idStelling: stellingId },
                     success: function(response) {
                         // Handle success response
-                        //console.log("partyid =" + partyId, "stellingid =" + stellingId);
+                        //console.log("partyid =" + partyId , "stellingid =" + stellingId);
                         console.log(response);
                         // Reload the page or update the table as needed
                         location.reload(); // For example, reload the page
@@ -611,8 +566,5 @@ ob_start();?>
         });
     });
 </script>
-
-
-
 </body>
 </html>
